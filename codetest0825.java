c6936346034033411ed567b7a97f6a82d40ce24a
JiHyeon kim <109847089+wooodii@users.noreply.github.com>
2022-08-24 23:10:50 +0900

    commit -m "a/b 조건받고 계산"

---
+++ codetest0825.java
@@ -0,0 +1,16 @@
+// A,B를 입력받고 A-B를 출력하세요
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        int A = in.nextInt();
+        int B = in.nextInt();
+        
+        System.out.println(A-B);
+        
+        in.close();
+
+    }
+}
