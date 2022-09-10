8ef0ece918a0e7dde52d5098e79c95d00bfc4a1a
JiHyeon kim <109847089+wooodii@users.noreply.github.com>
2022-09-10 17:08:01 +0900

    input class

---
+++ cdt0910/abminus.java
@@ -0,0 +1,20 @@
+package cdt0910;
+import java.util.Scanner;
+
+public class abminus {
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        
+        int T = sc.nextInt();
+
+        for(int i=1; i<=T; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            System.out.println("Case #"+i+": "+(a+b));
+        }
+        sc.close();
+    }
+    
+}
