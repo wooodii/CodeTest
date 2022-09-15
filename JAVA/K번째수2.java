c11caf8c504d2a5e87b71c4633f2c6202e054faa
JiHyeon kim <jihyeonkim1@naver.com>
2022-09-15 17:20:28 +0900

    quick sort

---
+++ JAVA/K번째수2.java
@@ -0,0 +1,56 @@
+package JAVA;
+
+public class K번째수2 {
+    // K번째 수
+    // 라이브러리를 이용하지 않고 퀵 정렬로 푼 문제
+    class Solution {
+        public int[] solution(int[] array, int[][] commands) {
+            int n = 0;
+            int[] ret = new int[commands.length];
+
+            while (n < commands.length) {
+                int m = commands[n][1] - commands[n][0] + 1;
+
+                if (m == 1) {
+                    ret[n] = array[commands[n++][0] - 1];
+                    continue;
+                }
+
+                int[] a = new int[m];
+                int j = 0;
+                for (int i = commands[n][0] - 1; i < commands[n][1]; i++)
+                    a[j++] = array[i];
+
+                sort(a, 0, m - 1);
+
+                ret[n] = a[commands[n++][2] - 1];
+            }
+            return ret;
+        }
+
+        void sort(int[] a, int left, int right) {
+            int pl = left;
+            int pr = right;
+            int x = a[(pl + pr) / 2];
+
+            do {
+                while (a[pl] < x)
+                    pl++;
+                while (a[pr] > x)
+                    pr--;
+                if (pl <= pr) {
+                    int temp = a[pl];
+                    a[pl] = a[pr];
+                    a[pr] = temp;
+                    pl++;
+                    pr--;
+                }
+            } while (pl <= pr);
+
+            if (left < pr)
+                sort(a, left, pr);
+            if (right > pl)
+                sort(a, pl, right);
+        }
+    }
+}
