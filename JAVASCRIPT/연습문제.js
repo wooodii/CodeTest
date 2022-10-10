e0f0199b894eadafa4164f60c257811a4f4188f4
JiHyeon kim <109847089+wooodii@users.noreply.github.com>
2022-10-10 17:45:11 +0900

    ()?( ? ):()

--- JAVASCRIPT/연습문제.js
+++ JAVASCRIPT/연습문제.js
@@ -1,4 +1,6 @@
 // 221009 프로그래머스 나이 출력
+// 221010 프로그램머스 할인비율 
+
 function solution(age) {
     var answer = 0;
     answer = 2022-age+1;
@@ -37,4 +39,30 @@ function solution(num_list) {
         answer.push(num_list[j-i])
     }
     return answer;
+}
+
+
+//옷가게 할인 받기
+function solution(price) {
+    if (price >= 500000) return Math.floor(price * 0.8);
+    if (price >= 300000) return Math.floor(price * 0.9);
+    if (price >= 100000) return Math.floor(price * 0.95);
+    return price;
+  }
+
+
+  // trunc
+function solution(price) {
+
+    let arr_discount = [[500000, 0.2], [300000, 0.1], [100000, 0.05]]
+
+    for(let i = 0 ; i < arr_discount.length ; i++)
+        if(arr_discount[i][0] <= price) return Math.trunc(price * (1-arr_discount[i][1]))        
+
+    return price
+}
+
+//삼항연산, 분수 곱
+function solution(price) {
+    return price>=500000 ? parseInt(price*8/10) : (price>=300000 ? parseInt(price*9/10) : (price >= 100000 ? parseInt(price*19/20) : price))
 }
